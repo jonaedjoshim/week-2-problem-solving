b2131980554fd41ca577f9d988dca409b3b69472
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-09-03 20:49:30 +0600

    week 2: day 4: problem H

---
+++ B_Radio_Station.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    map<string, string> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name, ip;
+        cin >> name >> ip;
+        mp[ip] = name;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        string command, ip;
+        cin >> command >> ip;
+
+        ip.pop_back();
+
+        cout << command << " " << ip << "; #" << mp[ip] << endl;
+    }
+
+    return 0;
+}
